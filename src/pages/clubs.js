572080402846286017be054159cd0fcded053b16
LordSamSire <100038599@csisd.org>
2023-09-22 15:18:54 -0500

    Tried to ake add club button work, Changed ClubListing to function from class

--- src/pages/clubs.js
+++ src/pages/clubs.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import TigerLogo from './TigerLogo.png';
 
-export class ClubListing extends React.Component{
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <span id="clist">
-                <p id="cname">{this.props.ctitle}</p>
-                <p id="chost">Club Sponsor: {this.props.sponsor}</p>
-                <p id="cdesc">{this.props.descript}</p>
-                <img id="cimg" src={this.props.clogo} />
-                <p id="cmeet">Next Meeting: {this.props.nextmeet}</p>
-                <p id="croom">Meeting in Room: {this.props.roomnum}</p>
-            </span>
-        );
-    }
+function ClubListing({ ctitle, sponsor, descript, clogo, nextmeet, roomnum }){
+    return (
+        <span id="clist">
+            <p id="cname">{ctitle}</p>
+            <p id="chost">Club Sponsor: {sponsor}</p>
+            <p id="cdesc">{descript}</p>
+            <img id="cimg" src={clogo} />
+            <p id="cmeet">Next Meeting: {nextmeet}</p>
+            <p id="croom">Meeting in Room: {roomnum}</p>
+        </span>
+    );
 }
 
 function addClub(){
-    
+    const club = document.createElement("ClubListing");
+    club.ctitle = "Robotics"
+    club.setAttribute('ctitle', 'Robotics');
+    club.setAttribute('cdesc', '');
+    document.getElementById("ClubsPage").appendChild(club)
 }
 
 
 export default function ClubsPage(){
     return(
         <div id="ClubsPage">
+            
             <h4>Clubs</h4>
             <button id="ccreate" onClick={addClub}>Add Club</button>
             <ClubListing ctitle="Newspaper Club" descript="The Roar is our schools newspaper that shines light on student researched and written topics." clogo={TigerLogo} sponsor="Mr. Williams" nextmeet="Tommorrow" roomnum="2303"/>
         </div>
     )
 }
+/*<form>
+    <input type="text" id="userin" defaultValue="Title" />
+    <input type="text" id="userin" defaultValue="Club Name" />
+    <input type="text" id="userin" defaultValue="Club Sponsor" />
+    <input type="text" id="userin" defaultValue="Short Club Description" />
+    <input type="text" id="userin" defaultValue="Next Meeting Date" />
+    <input type="text" id="userin" defaultValue="Meeting Room Number" />
+</form>
+*/
--- src/pages/login.js
+++ src/pages/login.js
@@ -8,8 +8,8 @@ export default function Login(){
         <div id="LoginPage">
             <h5>Login to HAC</h5>
             <form onSubmit={handleSubmit}>
-                <input type="text" id="userin" defaultValue="Username"></input>
-                <input id="passin" defaultValue="Password"></input>
+                <input type="text" id="userin" defaultValue="Username" />
+                <input id="passin" defaultValue="Password" />
                 <button type="submit" id="submitlogin">Login</button>
             </form>
         </div>
