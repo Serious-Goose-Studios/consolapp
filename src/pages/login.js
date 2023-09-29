3652f38265cac7e7c97e89c246376ef15ca6bbaf
LordSamSire <100038599@csisd.org>
2023-09-29 15:50:23 -0500

    Made Log in page nicer

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,9 +21,15 @@ export default function Login(){
         <div id="LoginPage">
             <h5>Login to HAC</h5>
             <form id="HACLogin" onSubmit={APIGetRequest}>
-                <input type="text" id="userin" defaultValue="Username" />
-                <input id="passin" defaultValue="Password" />
-                <button type="submit" id="submitlogin">Login</button>
+                <label id="userField">
+                    Username
+                    <input type="text" id="userin" defaultValue="" />
+                </label>
+                <label id="passField">
+                    Password
+                    <input type="password" id="passin" defaultValue="" />
+                </label>
+                <button type="submit" class="submitLogin" id="submitlogin">Login</button>
             </form>
         </div>
     )
